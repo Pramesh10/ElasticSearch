a0897d8c4cf9bbf466ad53eaddf4e66ad5dc123c
Pramesh KC <kcpramesh123@hotmail.com>
2024-07-24 21:43:08 +0545

    test changes

--- ElasticSearch/Controllers/FilesController.cs
+++ ElasticSearch/Controllers/FilesController.cs
@@ -54,7 +54,7 @@ namespace ElasticSearch.Controllers
 
                     ContentExtraction ce = new ContentExtraction();
                     await ce.ConvertToContent(file);
-                    
+                    Console.WriteLine("hellotest");
                     return Ok(file);
                 }
                 else
